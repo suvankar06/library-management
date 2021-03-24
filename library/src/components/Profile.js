3ec35bf047bdb42da86ff58287f6ac419182fca2
suvankar singh <suvankarxavi.singh06@gmail.com>
2021-03-24 11:49:03 +0530

    profile page updated

--- library/src/components/Profile.js
+++ library/src/components/Profile.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import ProfileUi from 'react-profile-card';
- 
- 
-function Example() {
-  return (
-    <div className='profile'> 
-      ygkjgkh
+//import { Link } from 'react-router-dom';
+import './Profile.css'
+
+function Profile()
+{
+  return(
+  <>
+  <div className='card'>
+  <img src='images/image10.jpg' alt=""/>
+  <p className="users-card__name">Suvankar Singh</p>
+  <p className="users-card__username">Suvankar@06</p>
+  <div className="users-card__divider"></div>
+  <div className="users-card__stats">
+    <div>
+    <span>Email</span>
+      <p>email@email.com</p>
+      
     </div>
-  );
+    <div>
+    <span>Mobile</span>
+      <p>6290186139</p>
+      
+    </div>
+    
+  </div>
+  </div>
+  </>
+  )
 }
- 
-export default Example;
+
+export default Profile;
